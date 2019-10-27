e0a3844227cbfe3e8c997e7961bc36187d8b401f
by <just_by@naver.com>
2019-10-27 19:47:28 +0900

    delete build

---
+++ app/src/main/java/com/otlab/here/LoginActivity.java
@@ -0,0 +1,58 @@
+package com.otlab.here;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText idText;
+    private EditText pwdText;
+    private CheckBox checkBox;
+    private Button loginBtn;
+
+    private SharedPreferences appData;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        // 설정값 불러오기
+        appData = getSharedPreferences("appData", MODE_PRIVATE);
+
+        idText = findViewById(R.id.idText);
+        pwdText = findViewById(R.id.pwdText);
+        checkBox = findViewById(R.id.checkBox);
+        loginBtn = findViewById(R.id.loginBtn);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 로그인 성공시 저장 처리, 예제는 무조건 저장
+                save();
+            }
+        });
+    }
+
+    // 설정값을 저장하는 함수
+    private void save() {
+        // SharedPreferences 객체만으론 저장 불가능 Editor 사용
+        SharedPreferences.Editor editor = appData.edit();
+
+        // 에디터객체.put타입( 저장시킬 이름, 저장시킬 값 )
+        // 저장시킬 이름이 이미 존재하면 덮어씌움
+        editor.putBoolean("SAVE_LOGIN_DATA", checkBox.isChecked());
+        editor.putString("ID", idText.getText().toString().trim());
+        editor.putString("PWD", pwdText.getText().toString().trim());
+
+        // apply, commit 을 안하면 변경된 내용이 저장되지 않음
+        editor.apply();
+    }
+
+}
--- app/src/main/java/com/otlab/here/MapActivity.java
+++ app/src/main/java/com/otlab/here/MapActivity.java
@@ -1,31 +1,123 @@
 package com.otlab.here;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import net.daum.mf.map.api.MapPOIItem;
+import net.daum.mf.map.api.MapPoint;
 import net.daum.mf.map.api.MapView;
 
 public class MapActivity extends Activity {
+
+    ViewGroup mapViewContainer = null;
+    private MapView mapView = null;
+    private MapPoint myMapPoint = null;
+    private MapPoint desMapPoint = null;
+    private MapPOIItem myMarker = null;
+    private MapPOIItem[] desMarker = new MapPOIItem[1];
+    private Location location = null;
+    private Handler handler = null;
+    final LocationListener gpsLocationListener = new LocationListener() {
+        public void onLocationChanged(Location location) {
+            refreshPosition();
+        }
+
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+        }
+
+        public void onProviderEnabled(String provider) {
+        }
+
+        public void onProviderDisabled(String provider) {
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstance) {
         super.onCreate(savedInstance);
         setContentView(R.layout.activity_map);
-        Toast.makeText(getApplicationContext(), "ㅅㄷㄴㅅ", Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(), "success", Toast.LENGTH_SHORT).show();
+        final LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        handler = new Handler();
+
 
         try {
 
-            MapView mapView = new MapView(this);
 
-            ViewGroup mapViewContainer = findViewById(R.id.map_view);
-            mapViewContainer.addView(mapView);
+            if (Build.VERSION.SDK_INT >= 23
+                    && ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                    && ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 0);
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 0);
+            } else {
 
+
+                location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, gpsLocationListener);
+                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000, 1, gpsLocationListener);
+
+                myMapPoint = MapPoint.mapPointWithGeoCoord(location.getLatitude(), location.getLongitude());
+
+                mapView = new MapView(this);
+                mapViewContainer = findViewById(R.id.map_view);
+                mapView.setMapCenterPoint(myMapPoint, false);
+                mapViewContainer.addView(mapView);
+
+
+                myMarker = new MapPOIItem();
+                newMarker("내 위치", myMapPoint, mapView, myMarker);
+
+                desMapPoint = MapPoint.mapPointWithGeoCoord(37.576947, 126.976830);
+                desMarker[0] = new MapPOIItem();
+                newMarker("상대 위치", desMapPoint, mapView, desMarker[0]);
+            }
         } catch (Exception e) {
             this.recreate();
             Log.d("??", e.getMessage());
             Toast.makeText(getApplicationContext(), "GPS 연결에 실패하였습니다.", Toast.LENGTH_LONG).show();
         }
+    }
+
+    private void refreshPosition() {
+        try {
+            myMarker.setMapPoint(MapPoint.mapPointWithGeoCoord(location.getLatitude(), location.getLongitude()));
+
+            Task task = new Task(handler, myMarker, desMarker);
+            task.execute();
+
+        } catch (Exception e) {
+        }
+    }
+
+    private void newMarker(String name, MapPoint mapPoint, MapView mapView, MapPOIItem marker) {
+        try {
+            marker.setItemName(name);
+            marker.setTag(0);
+            marker.setMapPoint(mapPoint);
+            marker.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin);
+            mapView.addPOIItem(marker);
+            mapView.setZoomLevel(1, true);
+        } catch (Exception e) {
+        }
+    }
+
+    public void onPause() {
+        super.onPause();
+        finish();
     }
 }
